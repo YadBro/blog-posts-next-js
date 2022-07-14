ffefb23d8e5fde2a27bcaf1ac9e9bf9c417591c0
YadBro <ncada708@gmail.com>
2022-07-14 20:57:13 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import styles from '@/styles/Home.module.css'
 
 export default function Home() {
   return (
